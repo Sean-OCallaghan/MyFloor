713f8683468441c45d193b8f9a14c66ff62febec
Sean <seanoinca@gmail.com>
2020-05-31 21:54:33 -0700

    Firebase
    added login system with basic functions

---
+++ login.js
@@ -0,0 +1,126 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBipQvSsn9GB2kcmMO2dN5trnHdtBPAC1Q",
+  authDomain: "myfloor-521c5.firebaseapp.com",
+  databaseURL: "https://myfloor-521c5.firebaseio.com",
+  projectId: "myfloor-521c5",
+  storageBucket: "myfloor-521c5.appspot.com",
+  messagingSenderId: "961576839952",
+  appId: "1:961576839952:web:4de72e86e19d4375855395",
+  measurementId: "G-D9MGZ3K8DH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+const auth = firebase.auth();
+
+function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    alert("Signed In");
+} 
+
+function signIn(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    alert("Signed In " + email.value);
+} 
+
+function signOut(){
+    auth.signOut();
+    alert("Signed Out");
+}
+
+auth.onAuthStateChanged(function(user){
+   
+    if(user){
+
+        var email = user.email;
+        alert("Active user " + email)
+        // window.location.href = "index.html"
+      }else{
+        alert("No Active User")
+       
+}
+});
+
+/*!
+    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+   (function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 71)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Floating label headings for the contact form
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+  })(jQuery); // End of use strict
+  
+
+
+
+
+    
